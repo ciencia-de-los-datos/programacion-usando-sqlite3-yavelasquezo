d8a8168e0fc52ecd248f8d21a2e249596daa8970
yavelasquezo <yavelasquezo@unal.edu.co>
2022-04-12 14:11:02 -0500

    Update pregunta_14.sql

--- pregunta_14.sql
+++ pregunta_14.sql
@@ -43,6 +43,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --  
-SELECT K0, AVG(c21)  FROM tbl2  INNER JOIN mibase  WHERE c13 > 400  GROUP BY K0, K1 
-CALIFICAME PUES
-NO VEZZZZZ Q    UE ESTOY DESESPERADAAA
+SELECT K0, AVG(c21)  FROM (tbl1 JOIN tbl2 ON tbl1.K1 =tbl2.K1)  WHERE c13 > 400  GROUP BY K0 
